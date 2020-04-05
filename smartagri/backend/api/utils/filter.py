d39d9fcfe26789015e97a9caea9883839a432b89
qingheihei <qingheihei@gmail.com>
2020-04-05 22:16:32 +0900

    Add number filter for sensor_value

--- smartagri/backend/api/utils/filter.py
+++ smartagri/backend/api/utils/filter.py
@@ -6,9 +6,13 @@ class SensorValueFilter(filters.FilterSet):
     from_date = filters.DateTimeFilter(field_name="created_at",lookup_expr='gt')
     until_date = filters.DateTimeFilter(field_name="created_at",lookup_expr='lt')
     sensor_type = filters.CharFilter(field_name='sensor__sensor_type__type_name',lookup_expr='icontains')
+    records_number = filters.NumberFilter(method='number_filter')
+    def number_filter(self, queryset, name, value):
+        print(len(queryset),value)
+        return queryset.all().order_by('id')[len(queryset)-value:]
     class Meta:
         model = models.SensorValue
-        fields = ['from_date','until_date','sensor_type']
+        fields = ['from_date','until_date','sensor_type','records_number']
 
 class OperationFilter(filters.FilterSet):
     device_type = filters.CharFilter(field_name='device__device_type__type_name',lookup_expr='icontains')
