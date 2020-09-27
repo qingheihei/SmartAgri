22c0b7705d3cacab18b5d5fd74d4c40a33bb2895
qingheihei <qingheihei@gamil.com>
2020-09-27 20:53:48 +0900

    modify:crotab plan

--- smartagri/backend/restart_django.sh
+++ smartagri/backend/restart_django.sh
@@ -1,2 +1,3 @@
+echo $(date)" restart django server"
 ps -ef|awk 'BEGIN{}{if(match($8, /python3/))system("kill -9 " $2)}END{}'
-nohup python3 manage.py runserver 0.0.0.0:8888 >djo.out 2>&1 &
+nohup python3 /home/ubuntu/SmartAgri/smartagri/backend/manage.py runserver 0.0.0.0:8888 >/home/ubuntu/SmartAgri/smartagri/backend/logs/django_out.log 2>&1 &
