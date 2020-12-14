bd8558d63d99d1db49cf6c1fa6d4c109b86f6b0a
qingheihei <qingheihei@gmail.com>
2020-12-14 15:15:18 +0000

    clear

--- smartagri/backend/restart_django.sh
+++
@@ -1,3 +0,0 @@
-echo $(date)" restart django server"
-ps -ef|awk 'BEGIN{}{if(match($8, /python3/))system("kill -9 " $2)}END{}'
-nohup python3 /home/ubuntu/SmartAgri/smartagri/backend/manage.py runserver 0.0.0.0:8888 >/home/ubuntu/SmartAgri/smartagri/backend/logs/django_out.log 2>&1 &
--- smartagri/backend/stop.sh
+++
@@ -1,2 +0,0 @@
-ps -ef|awk 'BEGIN{}{if(match($8, /python3/))system("kill -9 " $2)}END{}'
-
